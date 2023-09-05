1628d9401d39ae28ed55417fd369cdf83300cd25
shaileshminsnapsys <75966330+shaileshminsnapsys@users.noreply.github.com>
2023-09-05 10:37:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import mkcert from 'vite-plugin-mkcert'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert(), basicSsl()],
+  plugins: [react()],
   server: {
     port: '443',
     https: {
       key: '/root/certification/key.pem',
-      cert: '/root/certification/cert.pem'
+      cert: '/root/certification/cert.pem',
     },
     proxy: {
-      
       '/socket.io': {
         target: 'https://66.42.104.128:8081',
-        ws: true
-      }
-    }
+        ws: true,
+      },
+    },
   },
 });
