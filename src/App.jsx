a1cd833ab77ab302a8d8a5fea5d34398fe769088
shaileshminsnapsys <shailesh.m.insnapsys@gmail.com>
2023-12-15 16:38:38 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { useState, useEffect, useRef } from 'react';
-import { BiSend } from 'react-icons/bi';
-import io from 'socket.io-client';
-import { FullmetalAPIURL, ChatBackendScoketUrl } from './config';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRedoAlt } from '@fortawesome/free-solid-svg-icons';
+
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
-import fetchModelsUtility from './util/FetchModels';
-import MarkDown from './components/markDown/MarkDown';
 import ChatPage from './components/layout/ChatPage';
-import ChatInput from './components/chat/ChatInput';
 function App() {
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,4 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './css/index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <App />
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
